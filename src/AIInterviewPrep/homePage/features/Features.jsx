911042d1bef267af8d87e72578a7d97c9d623dc1
Nandiniatri <atrinandini8@gmail.com>
2025-07-23 11:52:39 +0530

    make a json data in features data

--- src/AIInterviewPrep/homePage/features/Features.jsx
+++ src/AIInterviewPrep/homePage/features/Features.jsx
@@ -1,17 +1,52 @@
+import { useEffect, useState } from 'react';
 import './Features.css';
 
 const Features = () => {
+    const [cardData, setCardData] = useState([]);
+
+    const fetchFeaturesData = async () => {
+        try {
+            const response = await fetch('/public/data/featuresData.json');
+            const result = await response.json();
+            setCardData(result);
+        } catch (error) {
+            console.log('Network Problem');
+        }
+    }
+
+    useEffect(() => {
+        fetchFeaturesData();
+    }, [])
+
     return (
         <div className='features-main-div'>
             <div className='features-div-first'>
-
+                <div className="features-top">
+                    <h2>
+                        Powerful Features of <span>InterviewAI</span>
+                    </h2>
+                    <p>
+                        InterviewAI is packed with a comprehensive set of powerful features
+                        designed to optimize and streamline your hiring process.
+                        Our platform empowers you to make data‑driven decisions.
+                    </p>
+                </div>
             </div>
 
             <div className='features-div-second'>
-                
+                {cardData.map((item) => {
+                    return (
+                        <div>
+                            
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
 }
 
 export default Features;
+
+
+
