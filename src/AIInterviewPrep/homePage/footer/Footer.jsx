44770a7a5bf20e68c4436deb3c17b1c1fd1c26af
Nandiniatri <atrinandini8@gmail.com>
2025-07-23 16:54:05 +0530

    make a footer file

--- src/AIInterviewPrep/homePage/footer/Footer.jsx
+++ src/AIInterviewPrep/homePage/footer/Footer.jsx
@@ -1,31 +1,41 @@
+import './Footer.css';
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+
+
 const Footer = () => {
     return (
-        <div>
-            <div>
-                <h1>Technology to revolutionize your hiring process</h1>
-                <h1>InterviewAI</h1>
+        <div className="footer-main-container">
+            <div className="footer-top">
+                <h1 className="footer-heading">Technology to revolutionize <br /> your hiring process</h1>
+                <div className="footer-logo">InterviewAI</div>
             </div>
 
-            <div>
-                <nav>
-                    <ul>
-                        <li>Home</li>
-                        <li>Features</li>
-                        <li>Pricing</li>
-                        <li>Resources</li>
-                    </ul>
-                </nav>
+            <div className="footer-nav">
+                <ul>
+                    <li>Home</li>
+                    <li>Features</li>
+                    <li>Pricing</li>
+                    <li>Resources</li>
+                </ul>
+
+                <div className="footer-socials">
+                    <p><FaInstagram /></p>
+                    <p><FaFacebook /></p>
+                    <p><FaTwitter /></p>
+                    <p><FaWhatsapp /></p>
+                </div>
             </div>
 
-            <div>
-                <p>InterviewAI is a registered company in India. Company Number 123456. @2025 InterviewAI LTD.</p>
-                <p>Privacy | Cookies | Terms & Conditions</p>
+            <div className="footer-legal">
+                <p>InterviewAI is a registered company in Indonesia. Company Number 123456. ©2025 InterviewAI LTD. All rights reserved.</p>
+                <p className="footer-links">Privacy | Cookies | Terms & Conditions</p>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Footer;
-
-
 
