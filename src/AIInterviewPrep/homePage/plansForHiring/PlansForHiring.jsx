339b2eb14547352efb8f584c688b81879ce4b3da
Nandiniatri <atrinandini8@gmail.com>
2025-07-23 14:25:43 +0530

    make a planForHiring

--- src/AIInterviewPrep/homePage/plansForHiring/PlansForHiring.jsx
+++ src/AIInterviewPrep/homePage/plansForHiring/PlansForHiring.jsx
@@ -1,17 +1,40 @@
+import { useEffect, useState } from "react";
 import { FaToggleOff, FaToggleOn } from "react-icons/fa";
 
 const PlansForHiring = () => {
+    const [plansCard , setPlansCard] = useState([]);
+
+    const fetchHiringPlan = async() => {
+        try {
+            const response = await fetch('/data/hiringPlanData.json');
+            const result = await response.json();
+            setPlansCard(result);
+        } catch (error) {
+            console.log('Network Issue');
+        }
+    }
+
+    useEffect(() => {
+        fetchHiringPlan();
+    }, [])
+
     return (
         <>
             <div>
                 <h1>Choose the Perfect Plan for your Hiring Needs</h1>
                 <p>Our Basic Plan is ideal for startups and small businesses looking to optimize their hiring process.</p>
                 <p><FaToggleOff /></p>
-                <p>Yearly <span style={{color:'purple'}}>-20% OFF</span></p>
+                <p>Yearly <span style={{ color: 'purple' }}>-20% OFF</span></p>
             </div>
 
             <div>
-                
+                {plansCard.map((item) => {
+                    return (
+                        <div>
+                            
+                        </div>
+                    )
+                })}
             </div>
         </>
     )
