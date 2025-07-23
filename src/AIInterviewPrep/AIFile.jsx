06b8ada219922d2d78671556a3a7fa2d466fa0be
Nandiniatri <atrinandini8@gmail.com>
2025-07-23 14:19:22 +0530

    make a planForHiring

--- src/AIInterviewPrep/AIFile.jsx
+++ src/AIInterviewPrep/AIFile.jsx
@@ -1,6 +1,7 @@
 import Features from "./homePage/features/Features";
 import Header from "./homePage/header/Header";
 import InterviewTech from "./homePage/interviewTech/InterviewTech";
+import PlansForHiring from "./homePage/plansForHiring/PlansForHiring";
 import TheIntelligentFile from "./homePage/theIntelligentSolu/TheIntelligent";
 
 const AIMainFile = () => {
@@ -10,6 +11,7 @@ const AIMainFile = () => {
             <TheIntelligentFile />
             <InterviewTech />
             <Features />
+            <PlansForHiring />
         </>
     )
 }
---
+++ src/AIInterviewPrep/homePage/plansForHiring/PlansForHiring.jsx
@@ -0,0 +1,20 @@
+import { FaToggleOff, FaToggleOn } from "react-icons/fa";
+
+const PlansForHiring = () => {
+    return (
+        <>
+            <div>
+                <h1>Choose the Perfect Plan for your Hiring Needs</h1>
+                <p>Our Basic Plan is ideal for startups and small businesses looking to optimize their hiring process.</p>
+                <p><FaToggleOff /></p>
+                <p>Yearly <span style={{color:'purple'}}>-20% OFF</span></p>
+            </div>
+
+            <div>
+                
+            </div>
+        </>
+    )
+}
+
+export default PlansForHiring;
