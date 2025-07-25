7fa609a2b91d854fd721463a5ed9dc343259cd9f
Nandiniatri <atrinandini8@gmail.com>
2025-07-25 22:09:38 +0530

    dead lock

--- src/revision/Revision.jsx
+++ src/revision/Revision.jsx
@@ -4,6 +4,14 @@ const Revision = () => {
     return (
         <div className='revision-main-div'>
             <p>Hello</p>
+            <button>Hello Baccho</button>
+            <p>jab two or more transation wait krte hai to indefinify for each other to realse a lock so this is deadlock</p>
+            <ul>
+                <li>Prevention</li>
+                <li>Avoidance</li>
+                <li>Detection</li>
+                <li>Restore</li>
+            </ul>
         </div>
     )
 }
