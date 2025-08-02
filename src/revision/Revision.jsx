3cc1625971774a6d0a76170cc8693a7cffb9c2f2
Nandiniatri <atrinandini8@gmail.com>
2025-08-02 23:38:25 +0530

    make a fetching

--- src/revision/Revision.jsx
+++ src/revision/Revision.jsx
@@ -6,8 +6,9 @@ const Revision = () => {
 
     const fetchData = async() => {
         try {
-            const response = await fetch();
+            const response = await fetch('https://dummyjson.com/products');
             const result = await response.json();
+            console.log(result);
             setData(result);
         } catch (error) {
             console.log('Network error');
