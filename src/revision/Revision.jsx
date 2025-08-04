b0680e878bb86ac7cd17a9ad4a9e743d547a673e
Nandiniatri <atrinandini8@gmail.com>
2025-08-04 20:45:14 +0530

    remove all this

--- src/revision/Revision.jsx
+++ src/revision/Revision.jsx
@@ -1,59 +1,60 @@
-import { useEffect, useState } from 'react';
-import './Revision.css';
+// import { useEffect, useState } from 'react';
+// import './Revision.css';
 
-const Revision = () => {
-    const [data , setData] = useState([]);
+// const Revision = () => {
+//     const [data , setData] = useState([]);
 
-    const fetchData = async() => {
-        try {
-            const response = await fetch('https://dummyjson.com/products');
-            const result = await response.json();
-            console.log(result);
-            setData(result);
-        } catch (error) {
-            console.log('Network error');
-        }
-    }
+//     const fetchData = async() => {
+//         try {
+//             const response = await fetch('https://dummyjson.com/products');
+//             const result = await response.json();
+//             console.log(result);
+//             setData(result);
+//         } catch (error) {
+//             console.log('Network error');
+//         }
+//     }
 
-    useEffect(() => {
-        fetchData();
-    },[])
+//     useEffect(() => {
+//         fetchData();
+//     },[])
 
-    return (
-        <div className='revision-main-div'>
-            <p>Hello</p>
-            <button>Hello Baccho</button>
-            <p>jab two or more transation wait krte hai to indefinify for each other to realse a lock so this is deadlock</p>
-            <ul>
-                <li>Prevention</li>
-                <li>Avoidance</li>
-                <li>Detection</li>
-                <li>Restore</li>
-            </ul>
+//     return (
+//         <div className='revision-main-div'>
+//             <p>Hello</p>
+//             <button>Hello Baccho</button>
+//             <p>jab two or more transation wait krte hai to indefinify for each other to realse a lock so this is deadlock</p>
+//             <ul>
+//                 <li>Prevention</li>
+//                 <li>Avoidance</li>
+//                 <li>Detection</li>
+//                 <li>Restore</li>
+//             </ul>
 
-            <h1>Check Point</h1>
-            <p>it is a machism of DB . Which they save the current state Date in DB.
-                In check point is a restore point that ensure , where the system where crash so they automatically return to the last safe current. It is known as check point.
-            </p>
-            <ul>
-                <li>Manully</li>
-                <li>Auto-mated</li>
-                <li>Time-based</li>
-                <li>Transaction-based</li>
-            </ul>
+//             <h1>Check Point</h1>
+//             <p>it is a machism of DB . Which they save the current state Date in DB.
+//                 In check point is a restore point that ensure , where the system where crash so they automatically return to the last safe current. It is known as check point.
+//             </p>
+//             <ul>
+//                 <li>Manully</li>
+//                 <li>Auto-mated</li>
+//                 <li>Time-based</li>
+//                 <li>Transaction-based</li>
+//             </ul>
 
-            <button>Hello</button>
+//             <button>Hello</button>
 
-            <button>Hello Baccho</button>
-            <button>hello</button>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda, tenetur aperiam! Id mollitia voluptatibus necessitatibus, sed aspernatur dicta dignissimos maxime assumenda iste similique optio impedit culpa enim architecto velit laborum voluptas consequuntur nisi ipsa. Nemo dolore delectus fuga ipsum necessitatibus, illo, earum corporis, veritatis nam ex quisquam quas! Laborum sequi quia, libero nisi illum laudantium ratione, magni deserunt asperiores cum quaerat iste error. Ipsa dolores harum inventore, fuga corrupti placeat culpa, quis amet dolore ad quia quas, beatae officia assumenda delectus sequi aperiam sunt cupiditate omnis facilis reiciendis temporibus dolor vel error? Mollitia fugit aliquam veritatis? Aliquid cumque nostrum facere.</p>
-            <h1>Hello Baccho</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro deserunt est, debitis inventore accusamus molestias explicabo maxime similique veniam nostrum, placeat exercitationem itaque aliquid vitae? Suscipit voluptate vitae ea doloremque.</p>
-            <i>kjndkjnknsakjnk</i>
-            <button>Hello</button>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint doloribus suscipit vitae tempora quis eveniet unde dicta nihil consequatur eius. Atque dolor aperiam laboriosam in quaerat inventore nulla ipsum deserunt.</p>
-        </div>
-    )
-}
+//             <button>Hello Baccho</button>
+//             <button>hello</button>
+//             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda, tenetur aperiam! Id mollitia voluptatibus necessitatibus, sed aspernatur dicta dignissimos maxime assumenda iste similique optio impedit culpa enim architecto velit laborum voluptas consequuntur nisi ipsa. Nemo dolore delectus fuga ipsum necessitatibus, illo, earum corporis, veritatis nam ex quisquam quas! Laborum sequi quia, libero nisi illum laudantium ratione, magni deserunt asperiores cum quaerat iste error. Ipsa dolores harum inventore, fuga corrupti placeat culpa, quis amet dolore ad quia quas, beatae officia assumenda delectus sequi aperiam sunt cupiditate omnis facilis reiciendis temporibus dolor vel error? Mollitia fugit aliquam veritatis? Aliquid cumque nostrum facere.</p>
+//             <h1>Hello Baccho</h1>
+//             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro deserunt est, debitis inventore accusamus molestias explicabo maxime similique veniam nostrum, placeat exercitationem itaque aliquid vitae? Suscipit voluptate vitae ea doloremque.</p>
+//             <i>kjndkjnknsakjnk</i>
+//             <button>Hello</button>
+//             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint doloribus suscipit vitae tempora quis eveniet unde dicta nihil consequatur eius. Atque dolor aperiam laboriosam in quaerat inventore nulla ipsum deserunt.</p>
+//         </div>
+//     ) 
+// }
 
-export default Revision;
+// export default Revision;
+
