3c4ba522446b5f5cb40cbcd33e485e9ca5f73725
Nandiniatri <atrinandini8@gmail.com>
2025-08-16 16:48:29 +0530

    header responsive

--- src/contextApi/DatasContectApi.jsx
+++ src/contextApi/DatasContectApi.jsx
@@ -14,7 +14,6 @@ const DatasContextApi = ({ children }) => {
     const [selectedPosition, setSelectedPosition] = useState("");
 
     const handleStartPractice = () => {
-        // alert('hello ji')
         if (
             resumeName &&
             selectRoundsTitle &&
