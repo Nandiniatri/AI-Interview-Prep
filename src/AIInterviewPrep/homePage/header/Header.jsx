9d7658fdb89e3299264bf4e1714b3a3ab3d20b09
Nandiniatri <atrinandini8@gmail.com>
2025-08-22 14:54:16 +0530

    show h1 on signup

--- src/AIInterviewPrep/homePage/header/Header.jsx
+++ src/AIInterviewPrep/homePage/header/Header.jsx
@@ -3,24 +3,39 @@ import './RespHeader.css';
 import { FaCaretDown } from "react-icons/fa";
 import { CiSearch } from "react-icons/ci";
 import Button from '../../../componets/Button';
+import { useState } from 'react';
 
 const Header = () => {
+    const [showModal , setShowModal] = useState(false);
+
+    const handleSignUpModal = () => {
+        alert('Hello Signup');
+        setShowModal(true);
+    }
+
+
     return (
-        <header className="hero-header">
-            <div className="nav-container">
-                <div className="logo">InterviewAI</div>
-                <nav className="nav-links">
-                    <a href="#">Home</a>
-                    <a href="#">Features</a>
-                    <a href="#">Pricing</a>
-                    <a href="#">Resources</a>
-                </nav>
-                <div className="auth-buttons">
-                    <Button className="login-btn">Contact Us</Button>
-                    <Button className="signup-btn">Sign Up</Button>
+        <>
+            <header className="hero-header">
+                <div className="nav-container">
+                    <div className="logo">InterviewAI</div>
+                    <nav className="nav-links">
+                        <a href="#">Home</a>
+                        <a href="#">Features</a>
+                        <a href="#">Pricing</a>
+                        <a href="#">Resources</a>
+                    </nav>
+                    <div className="auth-buttons">
+                        <Button className="login-btn">Contact Us</Button>
+                        <Button className="signup-btn" onClick={handleSignUpModal}>Sign Up</Button>
+                    </div>
                 </div>
+            </header>
+
+            <div>
+                {showModal && <h1>Hello</h1>}
             </div>
-        </header>
+        </>
     );
 };
 
