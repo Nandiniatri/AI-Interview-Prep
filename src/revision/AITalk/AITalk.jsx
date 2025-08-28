2d0ae845f58aec6d432b7e97b26744cc0f57d46a
Nandiniatri <atrinandini8@gmail.com>
2025-08-28 17:01:22 +0530

    mene avatar bana diya yeeeeeeeeeeeeeeeeeeeeee

--- src/revision/AITalk/AITalk.jsx
+++ src/revision/AITalk/AITalk.jsx
@@ -107,19 +107,20 @@ import { OrbitControls, useGLTF } from "@react-three/drei";
 import './AITalk.css';
 
 function AvatarModel() {
+  // local file ka path
   const { scene } = useGLTF("/data/avatar/avatar.glb");
   console.log(scene);
   
 
-  return <primitive object={scene} scale={3} position={[0, -3, 2]} />;
+  return <primitive object={scene} scale={2} position={[0, -1, 0]} />;
 }
 
 export default function AvatarViewer() {
   return (
     <div style={{ width: "100%", height: "500px" }}>
-      <Canvas className="canvas-AvatarModal">
+      <Canvas camera={{ position: [0, 1, 3] }} className="canvas-AvatarModal">
         <ambientLight intensity={0.5} />
-        <directionalLight position={[0, 0, 7]} />
+        <directionalLight position={[2, 2, 2]} />
         <Suspense fallback={null}>
           <AvatarModel />
         </Suspense>
