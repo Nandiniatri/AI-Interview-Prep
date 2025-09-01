e2723f450749dcaf4813b503fe0bfa36b514b55a
Nandiniatri <atrinandini8@gmail.com>
2025-09-01 15:27:18 +0530

    make a simple UI

--- src/revision/AiTalkAgain/AiTalkAgain.jsx
+++ src/revision/AiTalkAgain/AiTalkAgain.jsx
@@ -3,29 +3,18 @@ import "./AiTalkAgain.css";
 
 function AiTalkAgain() {
     return (
-        <div className="app">
-            {/* Left side - Interviewer Video */}
+        <div className="Ai-talk-again-app">
             <div className="video-section">
-                <video width="400" controls>
-                    <source src="/interviewer.mp4" type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-                <button className="end-btn">End Answer</button>
-                <p className="question">What's a skill you're currently working on improving?</p>
+                <div className="video-genrate">
+
+                </div>
             </div>
 
-            {/* Right side - Candidate Info */}
-            <div className="sidebar">
-                <img
-                    src="/candidate.jpg"
-                    alt="Candidate"
-                    className="candidate-img"
-                />
-                <h3>UI Designer</h3>
-                <p>Warm Up</p>
-                <button className="criteria-btn">Evaluation Criteria</button>
-                <button className="exit-btn">Exit Interview</button>
+            <div className="video-call-btn">
+                <button className="start-call">Start Call</button>
+                <button className="end-btn">End Answer</button>
             </div>
+
         </div>
     );
 }
