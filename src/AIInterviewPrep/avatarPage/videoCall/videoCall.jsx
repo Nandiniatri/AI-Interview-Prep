d448c2e358199242cf9481d87f1033f84b3b92c5
Nandiniatri <atrinandini8@gmail.com>
2025-09-08 20:04:07 +0530

    update code

--- src/AIInterviewPrep/avatarPage/videoCall/videoCall.jsx
+++ src/AIInterviewPrep/avatarPage/videoCall/videoCall.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useRef } from 'react';
+import './VideoCall.css';
+
 const VideoCall = () => {
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(stream => {
+                if (videoRef.current) {
+                    videoRef.current.srcObject = stream
+                }
+            })
+            .catch(err => {
+                console.log('Camera or mic not working', err);
+            })
+
+    }, [])
+
     return (
-        <div>
-            <h1>Hello I am a video Call</h1>
+        <div className="video-Call-main-div">
+            <video ref={videoRef} autoPlay playsInline muted className='video-call'/>
         </div>
     )
 }
