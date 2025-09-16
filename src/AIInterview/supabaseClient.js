c3e0404f41919d732584e46a4aadf91c303da3a7
Nandiniatri <atrinandini8@gmail.com>
2025-09-16 16:07:56 +0530

    ek error aa rahi thi supabase ki bcz mene jo implement kiya tha wo // import { createClient } from @supabase/supabase-js;
    // const supabaseUrl = https://rcoxcxrdamivrewlpybx.supabase.co;
    // const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjb3hjeHJkYW1pdnJld2xweWJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1OTUzNzcsImV4cCI6MjA3MTE3MTM3N30.nCb3IMC74777MxFqPdtu2Bk_dg_uMR5ij6BUT2lcgvY;
    
    // export const supabase = createClient(supabaseUrl, supabaseKey);
     tha or ye kiya ab import { createClient } from @supabase/supabase-js;
    
    const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
    const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
    
    export const supabase = createClient(supabaseUrl, supabaseKey);
     abhi smjh ni ho raha hai ki kese krre

--- src/AIInterview/supabaseClient.js
+++ src/AIInterview/supabaseClient.js
@@ -1,25 +1,15 @@
 // import { createClient } from "@supabase/supabase-js";
 
 // const supabaseUrl = "https://rcoxcxrdamivrewlpybx.supabase.co";
-// const supabaseAnonKey  = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjb3hjeHJkYW1pdnJld2xweWJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1OTUzNzcsImV4cCI6MjA3MTE3MTM3N30.nCb3IMC74777MxFqPdtu2Bk_dg_uMR5ij6BUT2lcgvY"
+// const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjb3hjeHJkYW1pdnJld2xweWJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1OTUzNzcsImV4cCI6MjA3MTE3MTM3N30.nCb3IMC74777MxFqPdtu2Bk_dg_uMR5ij6BUT2lcgvY";
 
-// export const supabase = createClient(supabaseAnonKey , supabaseUrl);
-
-
-
-// import { createClient } from "@supabase/supabase-js";
-
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-
+// export const supabase = createClient(supabaseUrl, supabaseKey);
 
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rcoxcxrdamivrewlpybx.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjb3hjeHJkYW1pdnJld2xweWJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1OTUzNzcsImV4cCI6MjA3MTE3MTM3N30.nCb3IMC74777MxFqPdtu2Bk_dg_uMR5ij6BUT2lcgvY";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
