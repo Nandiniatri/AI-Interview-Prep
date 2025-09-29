e82dea53e8a71148d40398a073e4cc95c87e4041
Nandiniatri <atrinandini8@gmail.com>
2025-09-29 23:02:43 +0530

    make a header responsive

--- src/AIInterviewPrep/homePage/header/Header.jsx
+++ src/AIInterviewPrep/homePage/header/Header.jsx
@@ -1,78 +1,3 @@
-// import './Header.css';
-// import './RespHeader.css';
-// import Button from '../../../componets/Button';
-// import { Link } from 'react-router-dom';
-// import { useDataContext } from '../../../contextApi/DatasContectApi';
-// import Image from '../../../componets/Image';
-// import { IoChevronDownSharp } from "react-icons/io5";
-// import { useState } from 'react';
-// import Modal2 from '../../../componets/modal2/Modal2';
-
-// const Header = () => {
-//     const { session, signOut } = useDataContext();
-//     const [isModalOpen , setIsModalOpen] = useState(false);
-
-//     const handleModal2 = () => {
-//         setIsModalOpen(true);
-//     };
-
-//     const handleCloseModal2 = () => {
-//         setIsModalOpen(false);
-//     };
-
-//     const handleLogout = () => {
-//         signOut();    
-//         handleCloseModal2();
-//     };
-
-//     return (
-//         <>
-//             <header className="hero-header">
-//                 <div className="nav-container">
-//                     <div className="logo">InterviewAI</div>
-//                     <nav className="nav-links">
-//                         <a href="#">Home</a>
-//                         <a href="#">Features</a>
-//                         <a href="#">Pricing</a>
-//                         <a href="#">Resources</a>
-//                     </nav>
-//                     <div className="auth-buttons">
-//                         <Button className="login-btn">Contact Us</Button>
-//                         {!session ? (
-//                             <Button className="signup-btn">
-//                                 <Link to="/login" className='inside-signup-btn'>
-//                                     Sign Up
-//                                 </Link>
-//                             </Button>
-//                         ) : (
-//                             <div className='header-auth-div'>
-//                                 <Image 
-//                                     src={session?.user?.user_metadata?.avatar_url} 
-//                                     className='header-auth-img' 
-//                                 />
-//                                 <span onClick={handleModal2}>
-//                                     {session?.user?.user_metadata?.name}
-//                                     <IoChevronDownSharp className='down-icon-header' size={'17px'} />
-//                                 </span>
-//                             </div>
-//                         )}
-//                     </div>
-//                 </div>
-
-//                 <Modal2 isOpen={isModalOpen} onClose={handleCloseModal2}>
-//                     <h4 onClick={handleLogout} style={{cursor: "pointer"}}>
-//                         Logout
-//                     </h4>
-//                 </Modal2>
-//             </header>
-//         </>
-//     );
-// };
-
-// export default Header;
-
-
-
 import './Header.css';
 import './RespHeader.css';
 import Button from '../../../componets/Button';
@@ -82,11 +7,17 @@ import Image from '../../../componets/Image';
 import { IoChevronDownSharp } from "react-icons/io5";
 import { useState } from 'react';
 import Modal2 from '../../../componets/modal2/Modal2';
+import { FiMenu, FiX } from "react-icons/fi";
 
 const Header = () => {
     const { session, signOut } = useDataContext();
     const location = useLocation();
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toogleMenu = () =>{
+        
+    }
 
     const handleModal2 = () => {
         setIsModalOpen(true);
@@ -101,7 +32,8 @@ const Header = () => {
         handleCloseModal2();
     };
 
-    const hideFullHeaderPages = ["/avatarModal", "/codingPage" , "/analysisPage"];
+
+    const hideFullHeaderPages = ["/avatarModal", "/codingPage", "/analysisPage"];
 
     const isHomePage = !hideFullHeaderPages.includes(location.pathname);
 
@@ -147,6 +79,10 @@ const Header = () => {
                         </Modal2>
                     </>
                 )}
+            </div>
+
+            <div className="menu-toggle" onClick={toogleMenu}>
+                <FiMenu />
             </div>
         </header>
     );
